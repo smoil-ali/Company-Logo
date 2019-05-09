d359a25401cf254e598e17e304f238cbadb8aaf3
smoil-ali <40652937+smoil-ali@users.noreply.github.com>
2019-05-09 08:10:06 -0700

    init commit

---
+++ Company Logo.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+from collections import Counter, OrderedDict
+
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+[print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
